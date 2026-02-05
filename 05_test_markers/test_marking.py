aa458827a3226bf991baf3475a85abfb5e5fa74b
jboegeholz <boegeholz.joern@gmail.com>
2026-02-05 15:39:19 +0100

    add more pytest tests

--- 05_test_markers/test_marking.py
+++ 05_test_markers/test_marking.py
@@ -2,16 +2,13 @@ import pytest
 
 
 @pytest.mark.windows
-def test_win_ver():
+def test_windows_version():
     pass
 
 @pytest.mark.linux
-def test_lin_ver():
+def test_linux_version():
     pass
 
 @pytest.mark.mac_os
 def test_stuff_mac_os():
-    pass
-
-def test_more_stuff_mac_os():
     pass
